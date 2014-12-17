7267f5ee3e448e304f41d97061176f640ac27e83
KaiManners <somethingweird102@gmail.com>
2014-12-16 22:42:29 -0500

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-function myFunction(){
-	var p = document.createElement("A");
-	p.setAttribute("href", "www.g4.com");
-	var t = document.createTextNode("Tutorials");
-	p.appendChild(t);
-	document.body.appendChild(p);
-}
-
